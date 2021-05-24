ac2902e2a04854c7f00572f352139899ab7fccf2
THE-ANALYST-17 <coolutkarsh.sinhg17@gmaiol.com>
2021-05-24 16:24:59 +0530

    assignment updated

---
+++ stp_watch/script.js
@@ -0,0 +1,60 @@
+const strt=document.querySelector(".start");
+const restrt=document.querySelector(".restart");
+const stp=document.querySelector(".stop");
+
+const min=document.querySelector(".min");
+const sec=document.querySelector(".sec");
+const millisec=document.querySelector(".milsec");
+
+let a;
+let s=0;
+let ms=0;
+let m=0;
+function startStopwatch(){
+    strt.disabled=true;
+    restrt.disabled=false;
+    stp.disabled=false;
+    ms++;
+   // millisec.innerHTML=ms;// for reference
+   millisec.innerHTML=(ms<=9) ? "0"+ms:ms
+    if(ms==100){
+        ms=0;
+        s++
+        if(s==60){
+            s=0;
+            m++
+           //min.innerHTML=m;
+           min.innerHTML=(m<=9) ? "0"+m:m;
+        }
+        // sec.innerHTML=s;
+        sec.innerHTML=(s<=9)  ?  "0"+s:s;
+    }
+  a=setTimeout(startStopwatch,10)
+}
+
+function stopStopwatch(){
+ clearTimeout(a);
+ strt.disabled=false;
+ restrt.disabled=false;
+ stp.disabled=true;
+}
+function restartStopwatch(){
+    
+    s=0;
+    ms=0;
+    m=0;
+    clearTimeout(a);
+    min.innerHTML="00";
+    sec.innerHTML="00";
+    millisec.innerHTML="00";
+    strt.disabled=false;
+    restrt.disabled=true;
+    stp.disabled=false;
+}
+
+
+
+
+strt.addEventListener("click",startStopwatch)
+stp.addEventListener("click",stopStopwatch)
+restrt.addEventListener("click",restartStopwatch)
